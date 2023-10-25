6036c23e7f8ad5c91b58e702108cf7f7720b5bad
Tien Ly <tntly@ucdavis.edu>
2023-10-24 23:12:12 -0700

    updated options of sbatch

--- fastqc.sh
+++ fastqc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name fastqc
+#SBATCH --output fastqc-%j.out
 #SBATCH --cpus-per-task 8
-#SBATCH --mem 8G
+#SBATCH --mem 10G
 
 # Change directories to where the fastq files are located
-cd /home/tly/wgs_pika/samples
+cd /home/tly/wgs-pika/samples
 
 # Load modules required for script commands
 module purge
 module load fastqc
 
 # Run FastQC
-fastqc -o /home/tly/wgs_pika/results/fastqc/ -t ${SLURM_CPUS_PER_TASK} *.fastq.gz
+fastqc -o /home/tly/wgs-pika/results/fastqc/ -t ${SLURM_CPUS_PER_TASK} *.fastq.gz
--- multiqc.sh
+++ multiqc.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name multiqc
+#SBATCH --output multiqc-%j.out
+#SBATCH --mem 10G
 
 # Change directories to where the MultiQC report will be located
-cd /home/tly/wgs_pika/results/multiqc/
+cd /home/tly/wgs-pika/results/multiqc/
 
 # Load modules required for script commands
 module purge
 module load intel-python3
 
 # Run MultiQC
-multiqc /home/tly/wgs_pika/results/fastqc/
+multiqc /home/tly/wgs-pika/results/fastqc/
