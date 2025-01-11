854d2215a67c2b7327c1eb89e00ef31ae184e8ff
Tien Ly <lnttien2000@gmail.com>
2025-01-10 18:34:16 -0800

    done with GEA analysis using BayPass

--- outlier-analysis/baypass.sh
+++ outlier-analysis/baypass.sh
@@ -18,23 +18,24 @@
 # Data:
 # - PED file: pika_73ind_4.8Msnp_10pop.ped
 # - Metadata file: pika_10pop_metadata.txt
+# - Environmental data files: pika_10pop_env1_baypass.txt, pika_10pop_env2_baypass.txt
 # --------------------------- #
 
 # --------------------------- #
-# Prepare input files
+# Prepare genotype data file
 # --------------------------- #
 # Set working directory
 DIR=~/outlier-analysis
 cd $DIR/baypass
 
 # Reorder metadata columns to match PLINK format (POP first, IND second)
-awk '{print $2 "\t" $1}' $DIR/data/pika_10pop_metadata.txt > pika_10pop_metadata_POPIND.txt
+awk '{print $2, $1}' $DIR/data/pika_10pop_metadata.txt > pika_10pop_metadata_POPIND.txt
 
 # Remove the first two columns (family ID and individual ID) from the PED file
 cut -d ' ' -f 3- $DIR/data/pika_73ind_4.8Msnp_10pop.ped > x.delete
 
 # Merge reordered metadata with the modified PED file
-paste pika_10pop_metadata_POPIND.txt x.delete > pika_73ind_4.8Msnp_10pop.ped
+paste -d ' ' pika_10pop_metadata_POPIND.txt x.delete > pika_73ind_4.8Msnp_10pop.ped
 rm x.delete
 
 # Copy required PLINK files
@@ -59,10 +60,60 @@ tail -n +2 pika_73ind_4.8Msnp_10pop.frq.strat | \
   sed 's/ /\n/20; P; D' > pika_10pop_geno_baypass.txt
 
 # --------------------------- #
-# Run BayPass core model to generate covariance matrix
+# Run BayPass core model
 # --------------------------- #
+# This step estimates the covariance matrix using the core model
 ~/programs/baypass_public-master/sources/g_baypass \
   -npop 10 \
   -gfile pika_10pop_geno_baypass.txt \
   -outprefix baypass-results/pika_10pop_baypass \
   -nthreads 16
+
+# --------------------------- #
+# Run BayPass under the AUX covariate model
+# --------------------------- #
+# GEA analysis 1: Using ppt_sum, vpdmin_min, and elev as covariates
+~/programs/baypass_public-master/sources/g_baypass \
+  -npop 10 \
+  -gfile pika_10pop_geno_baypass.txt \
+  -efile pika_10pop_env1_baypass.txt \
+  -auxmodel \
+  -omegafile baypass-results/pika_10pop_baypass_mat_omega.out \
+  -outprefix baypass-results/pika_10pop_baypass_gea1 \
+  -nthreads 16
+
+# GEA analysis 2: Using ppt_sum, vpdmin_min, and winter_tmax_max as covariates
+~/programs/baypass_public-master/sources/g_baypass \
+  -npop 10 \
+  -gfile pika_10pop_geno_baypass.txt \
+  -efile pika_10pop_env2_baypass.txt \
+  -auxmodel \
+  -omegafile baypass-results/pika_10pop_baypass_mat_omega.out \
+  -outprefix baypass-results/pika_10pop_baypass_gea2 \
+  -nthreads 16
+
+# --------------------------- #
+# Filter outliers with estimated Bayes Factors > 25
+# --------------------------- #
+# Filter outliers for GEA analysis 1
+cat baypass-results/pika_10pop_baypass_gea1_summary_betai.out | \
+  awk '$6 > 25' > baypass-results/pika_baypass_gea1_BF25.txt
+
+# Filter outliers for GEA analysis 2
+cat baypass-results/pika_10pop_baypass_gea2_summary_betai.out | \
+  awk '$6 > 25' > baypass-results/pika_baypass_gea2_BF25.txt
+
+# --------------------------- #
+# Identify outlier SNP IDs
+# --------------------------- #
+# Match filtered outliers from GEA 1 with SNP IDs
+awk 'FNR == NR {a[$2]; next} (($4) in a)' \
+  baypass-results/pika_baypass_gea1_BF25.txt \
+  $DIR/data/pika_10pop_SNPs.txt | \
+  cut -f 3 > baypass-results/pika_baypass_gea1_outlier_SNPIDs.txt
+
+# Match filtered outliers from GEA 2 with SNP IDs
+awk 'FNR == NR {a[$2]; next} (($4) in a)' \
+  baypass-results/pika_baypass_gea2_BF25.txt \
+  $DIR/data/pika_10pop_SNPs.txt | \
+  cut -f 3 > baypass-results/pika_baypass_gea2_outlier_SNPIDs.txt
